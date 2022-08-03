44fabd7009529c3e433069c8678ee0a3a7d6935c
AzizurRahmanEmon <47455161+AzizurRahmanEmon@users.noreply.github.com>
2022-08-03 10:49:19 +0600

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'> 
+      <h1>about us</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore maiores natus consequatur soluta, labore non eius eos ipsa nobis expedita quidem sit itaque laudantium tempora praesentium saepe, dignissimos accusantium nam odio esse. Quis, dolores rem. Hic ducimus quasi ipsum aliquam!</p>
+    </section>
+  )
+}
+
+export default About
